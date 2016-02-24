4a0a70cd23dc4424d7315dee080e165795b68aae
ouazou <zoubiroauarab@sympatico.ca>
2016-02-24 16:38:15 -0500

    working on the persistence testing

--- book-app/book-web/book-web-app/src/test/java/com/paymentpin/projects/book/app/dao/BookRepositoryTest.java
+++ book-app/book-web/book-web-app/src/test/java/com/paymentpin/projects/book/app/dao/BookRepositoryTest.java
@@ -1,12 +1,19 @@
 package com.paymentpin.projects.book.app.dao;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.paymentpin.projects.book.app.entities.Book;
 import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -15,6 +22,10 @@ import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/conf/application-conf.xml")
+@Transactional
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
 public class BookRepositoryTest {
 
     @Autowired
@@ -29,5 +40,19 @@ public class BookRepositoryTest {
         Assert.assertEquals(1,book2fetch.size());
 
     }
+
+
+
+
+    @Test
+    @DatabaseSetup("/database/books.xml")
+    public void testStore() throws Exception {
+
+        List<Book> book2fetch=repository.findByTitle("Recontre du troisieme type");
+        Assert.assertEquals(1,book2fetch.size());
+
+    }
+
+
 
 }
